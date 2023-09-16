99dc497cb135bc6e841f201a301fbb828bebfe0c
cbustoso <121638773+cbustoso@users.noreply.github.com>
2023-09-16 12:29:00 -0300

    output

---
+++ EC2/output.tf
@@ -0,0 +1,9 @@
+output "URL WEB "" {
+  description = "DNS pública del servidor"
+  value       = "http://${aws_instance.servidor_1.public_dns}:${var.puerto_servidor}"
+}
+
+output "dns_publica_server2" {
+  description = "DNS pública del servidor"
+  value       = "http://${aws_instance.servidor_2.public_dns}:${var.puerto_ssh}"
+}
